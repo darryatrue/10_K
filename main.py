0d1a013971db6e8140c16bf9f84e640f323ed223
Учащийся Школы 1581 <student-1581@cao.obr.mos.ru>
2022-04-01 15:31:53 +0300

    s

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-git push -u origin main# This is a sample Python script.
+# This is a sample Python script.
 print('hello git')
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
